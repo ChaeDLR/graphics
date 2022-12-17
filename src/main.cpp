de3236afb5706b5145d24518945c5019583209ed
ChaeDlr <delarosa.chae@gmail.com>
2022-12-17 10:12:44 -0500

    remove comment

--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,6 @@ struct {
   } Info;
 } App;
 
-// static struct Colors {
-//   const SDL_Color BLACK = {0, 0, 0, 255};
-//   const SDL_Color WHITE = {225, 225, 225, 255};
-//   const SDL_Color BLUE = {44, 116, 168, 255};
-//   const SDL_Color RED = {210, 116, 44, 255};
-// } Colors;
-
 uint16_t halfw;
 uint16_t halfh;
 
